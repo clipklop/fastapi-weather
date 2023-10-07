5e05ae680053e4344c3b39c73c560083ba3b24db
Mischa <mishayartsev@gmail.com>
2023-10-07 13:41:07 +0300

    wip for saving weather

--- parser/models/model.py
+++ parser/models/model.py
@@ -1,6 +1,28 @@
-import dataclasses
+import datetime
+from dataclasses import dataclass, field
 
 
-@dataclasses.dataclass
+@dataclass
 class Weather():
-    pass
+    city_name: str
+    country: str
+    sunrise: datetime.datetime
+    sunset: datetime.datetime
+    longitude: 37.6156
+    latitude: 55.7522
+    weather: 'Rain'
+    weather_description: 'небольшой дождь'
+    icon: '10d'
+    temperature: 4.09
+    feels_like: -0.76
+    temp_min: 2.79
+    temp_max: 4.65
+    pressure: 993
+    humidity: 91
+    sea_level: 993
+    grnd_level: 975
+    visibility: 10000
+    checked_at: datetime.datetime
+    wind: dict = field(default_factory=lambda: {'speed': 7.27, 'deg': 179, 'gust': 15.22})
+    rain: dict = field(default_factory=lambda: {'1h': 0.87})
+    clouds: dict = field(default_factory=lambda: {'all': 100})
--- parser/openweathermap.py
+++ parser/openweathermap.py
@@ -6,15 +6,17 @@ from models.model import Weather
 from config import OWP_API_KEY
 
 
-def get_weather(city_name: str, api_key: str, lang: str = 'ru') -> dict[str, str]:
-
+def get_weather(city_name: str, api_key: str, lang: str = 'ru') -> dict[str, str] | None:
     url = f"https://api.openweathermap.org/data/2.5/weather?&units=metric&q={city_name}&appid={api_key}&lang={lang}"
-    r = requests.get(url).json()
-
-    return r
+    
+    try:
+        weather = requests.get(url).json()
+        return weather
+    except Exception as e:
+        print(e)
+        return None
 
 
 if __name__ == '__main__':
     weather = get_weather(city_name='Moscow', api_key=OWP_API_KEY)
     print(weather)
-    print(OWP_API_KEY)
