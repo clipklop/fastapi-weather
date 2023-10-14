9ff701254ff92778ba3a36afca7464140c1c7c53
Mischa <mishayartsev@gmail.com>
2023-10-14 15:09:41 +0300

    +config with dataclass in load function

--- parser/config.py
+++ parser/config.py
@@ -1,7 +1,20 @@
+import imp
 import os
+import dataclasses
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-OWP_API_KEY = os.getenv("OWP_API_KEY")
+
+@dataclasses.dataclass
+class Config:
+    openweather_url: str | None
+    openweather_api_key: str | None
+
+
+def load() -> Config:
+    return Config(
+        openweather_url=os.getenv("OWP_URL"),
+        openweather_api_key=os.getenv("OWP_API_KEY")
+    )
--- parser/openweathermap.py
+++ parser/openweathermap.py
@@ -3,20 +3,31 @@ import json
 import requests
 
 from models.model import Weather
-from config import OWP_API_KEY
+from config import OWP_API_KEY, OWP_URL
 
 
-def get_weather(city_name: str, api_key: str, lang: str = 'ru') -> dict[str, str] | None:
-    url = f"https://api.openweathermap.org/data/2.5/weather?&units=metric&q={city_name}&appid={api_key}&lang={lang}"
-    
-    try:
-        weather = requests.get(url).json()
-        return weather
-    except Exception as e:
-        print(e)
-        return None
+class OpenWeatherClient:
+    def __init__(self, url: str, api_key: str) -> None:
+        self.url = url
+        self.api_key = api_key
+
+    def get_weather(self, city_name: str, lang: str = 'ru') -> dict[str, dict | str | list] | None:
+
+        if self.url is None or self.api_key is None:
+            return None
+
+        self.url = self.url + \
+            f"/weather?&units=metric&q={city_name}&appid={self.api_key}&lang={lang}"
+
+        try:
+            weather = requests.get(self.url).json()
+            return weather
+        except Exception as e:
+            print(e)
+            return None
 
 
 if __name__ == '__main__':
-    weather = get_weather(city_name='Moscow', api_key=OWP_API_KEY)
+    owc = OpenWeatherClient(OWP_URL, OWP_API_KEY)
+    weather = owc.get_weather(city_name='Moscow')
     print(weather)
