a5931212f0b4d3ee46b267646e11c1fbb2ee9785
Mischa <mishayartsev@gmail.com>
2023-10-16 01:00:38 +0300

    saving model func

--- parser/models/model.py
+++ parser/models/model.py
@@ -8,21 +8,21 @@ class Weather():
     country: str
     sunrise: datetime.datetime
     sunset: datetime.datetime
-    longitude: 37.6156
-    latitude: 55.7522
-    weather: 'Rain'
-    weather_description: 'небольшой дождь'
-    icon: '10d'
-    temperature: 4.09
-    feels_like: -0.76
-    temp_min: 2.79
-    temp_max: 4.65
-    pressure: 993
-    humidity: 91
-    sea_level: 993
-    grnd_level: 975
-    visibility: 10000
+    longitude: float
+    latitude: float
+    weather: str
+    weather_description: str
+    icon: str
+    temperature: float
+    feels_like: float
+    temp_min: float
+    temp_max: float
+    pressure: int
+    humidity: int
+    sea_level: int
+    grnd_level: int
+    visibility: int
     checked_at: datetime.datetime
-    wind: dict = field(default_factory=lambda: {'speed': 7.27, 'deg': 179, 'gust': 15.22})
-    rain: dict = field(default_factory=lambda: {'1h': 0.87})
-    clouds: dict = field(default_factory=lambda: {'all': 100})
+    wind: dict = field(default_factory=lambda: {'speed': float, 'deg': int, 'gust': float})
+    rain: dict = field(default_factory=lambda: {'1h': float})
+    clouds: dict = field(default_factory=lambda: {'all': int})
--- parser/openweathermap.py
+++ parser/openweathermap.py
@@ -1,7 +1,7 @@
 import json
+from datetime import datetime   
 
 import requests
-# import typeshed
 
 from config import load
 from models.model import Weather
@@ -12,20 +12,17 @@ class OpenWeatherClient:
         self.url = url
         self.api_key = api_key
 
-    def get_weather(self, city_name: str, lang: str = 'ru') -> dict[str, dict | str | list] | None:
+    def get_weather(self, city_name: str, lang: str = 'ru') -> dict[str, str]:
 
         if self.url is None or self.api_key is None:
             return None
 
-        # self.url = self.url + \
-        #     f"/weather?&units=metric&q={city_name}&appid={self.api_key}&lang={lang}"
         payload = {
             'q': city_name,
             'appid': self.api_key,
             'units': 'metric',
             'lang': lang
         }
-        print(self.url)
 
         try:
             weather = requests.get(self.url, params=payload).json()
@@ -35,8 +32,34 @@ class OpenWeatherClient:
             return None
 
 
+def weather_to_model(weather_dict: dict[str, str]) -> Weather:
+    return Weather(
+        city_name=weather_dict.get('name'),
+        country=weather_dict.get('sys').get('country'),
+        sunrise=datetime.fromtimestamp(weather_dict.get('sys').get('sunrise')),
+        sunset=datetime.fromtimestamp(weather_dict.get('sys').get('sunset')),
+        longitude=weather_dict.get('coord').get('lon'),
+        latitude=weather_dict.get('coord').get('lat'),
+        weather=weather_dict.get('weather')[0].get('main'),
+        weather_description=weather_dict.get('weather')[0].get('description'),
+        icon=weather_dict.get('weather')[0].get('icon'),
+        temperature=weather_dict.get('main').get('temp'),
+        feels_like=weather_dict.get('main').get('feels_like'),
+        temp_min=weather_dict.get('main').get('temp_min'),
+        temp_max=weather_dict.get('main').get('temp_max'),
+        pressure=weather_dict.get('main').get('pressure'),
+        humidity=weather_dict.get('main').get('humidity'),
+        sea_level=weather_dict.get('main').get('sea_level'),
+        grnd_level=weather_dict.get('main').get('grnd_level'),
+        visibility=weather_dict.get('visibility'),
+        checked_at=datetime.fromtimestamp(weather_dict.get('dt')),
+    )
+
+
 if __name__ == '__main__':
     config = load()
     owc = OpenWeatherClient(config.openweather_url, config.openweather_api_key)
     weather = owc.get_weather(city_name='Moscow')
     print(weather)
+    w = weather_to_model(weather)
+    print(w)
